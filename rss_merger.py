7a56e2f043fb38e25f7e6475a3cb3fc7f535109c
Ульяна <150696793+uahmatova@users.noreply.github.com>
2026-07-20 23:05:06 +0500

    Update rss_merger.py

--- rss_merger.py
+++ rss_merger.py
@@ -13,7 +13,8 @@ VK_GROUPS = [
     'cozmp59',
     'rpn_permkrai', 
     'gdkbpichugina',
-    'diabet_expert'
+    'diabet_expert',
+    'pkspkperm'
 ]
 
 #VK_TOKEN = 'd0aba882d0aba882d0aba8829cd39c235edd0abd0aba882b82c14340f4c8b620425b030'
